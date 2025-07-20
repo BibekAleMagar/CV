c14f3e9c595ff90020469e97f24f4c55b981a036
Bibek Ale Magar <magarbibek71@gmail.com>
2025-07-20 14:47:49 +0545

    Completed

---
+++ cv/src/App.jsx
@@ -0,0 +1,94 @@
+function App() {
+  
+
+  return (
+    <>
+     <div className="port">
+       <div className="heading">
+        <h1>Bibek Ale Magar</h1>
+        <h3>Frontend Developer</h3>
+        <p>+977-9810325091 | Gokarneshwor-5, Kathmandu, Nepal | <a href="mailto:magarbibek71@gmail.com">magarbibek71@gmail.com</a> | <a href="https://github.com/BibekAleMagar">GitHub</a> | <a href="https://www.linkedin.com/in/bibek-magar-7b0504255/" target="_blank">LinkedIn</a></p>
+      </div>
+      <div className="profile">
+        <h2>Profile</h2>
+        <hr />
+        <p>Innovative and detail-oriented Frontend Developer skilled in React.js. Adept at building responsive, high-
+            performance web applications with a focus on user experience. Seeking an internship or entry-level role to
+            apply and expand my frontend development skills in a collaborative environment. Passionate about
+            continuous learning and leveraging modern tools to solve real-world problems.</p>
+      </div>
+      <div className="skill">
+        <h2>Technical Skills</h2>
+        <hr />
+        <h3>Frontend</h3>
+        <ul>
+          <li>Proficient in HTML, CSS, JavaScript, React.js, Tailwind CSS, Bootstrap</li>
+        </ul>
+        <h3>Backend & Tools</h3>
+        <ul>
+          <li>Database: Firebase, MongoDB</li>
+          <li>Tools: GitHub</li>
+        </ul>
+      </div>
+      <div className="project">
+        <h2>Projects</h2>
+        <hr />
+        <div id="asalt">
+          <h3>Asalt Guidance</h3>
+          <p>HTML, CSS, JavaScript</p>
+          <ul>
+          <li>Developed a responsive and visually engaging platform built using HTML, CSS, and JavaScript, enhanced with
+            AOS for smooth scrolling animations.</li>
+          <li> Designed an intuitive layout, interactive elements, and a professional design to ensure a seamless user
+            experience.</li>
+          <li><a href="https://github.com/Aslat-Education/aslatguidance">GitHub</a> | <a href="https://aslat.netlify.app/">Live</a></li>
+        </ul>
+        </div>
+        <div id="blog">
+          <h3>Kendrix Blogs</h3>
+        <p>React js, Tailwind CSS, MongoDB, Express js</p>
+        <ul>
+          <li>Worked on MERN stack project that allows CRUD operations to manage blog posts and images.</li>
+          <li>Created a simple and efficient interface for creating, updating, and deleting content with smooth backend
+            integration.</li>
+          <li><a href="https://github.com/BibekAleMagar/KendrixBlogs">GitHub</a> | <a href="https://kendrix-blogs.vercel.app/">Live</a></li>
+        </ul>
+        </div>
+        <div id="track">
+          <h3>Expense Tracker</h3>
+        <p>React js, Tailwind CSS, Firebase</p>
+        <ul>
+          <li>Designed and developed a hospital management platform to streamline operations, featuring modern UI
+              and Firebase backend.</li>
+          <li>Created role-based dashboards for Admins, Doctors, and Patients, ensuring secure access and navigation.</li>
+          <li><a href="https://github.com/BibekAleMagar/Expense-Tracker">GitHub</a></li>
+        </ul>
+
+        </div>
+      </div>
+      <div className="education">
+        <h2>Education</h2>
+        <hr />
+        <div id="college">
+          <p>Texas College of Management and IT</p>
+          <p>Siphal, Kathmandu</p>
+        </div>
+        <div id="edu">
+          <p>Bachelor of Information Technology</p>
+          <p>Expected Nov 2026</p>
+        </div>
+        <p>Relevant Coursework: Web Development, Data Structure, Database System</p>
+
+      </div>
+      <div className="langauge">
+        <h2>Languages and Other Skills</h2>
+        <hr />
+        <p>Nepali (Native), English (Fluent)</p>
+        <p>Interpersonal Skills: Persuasive Presentation, Collaborative leadership, Troubleshooting, Articulate Communication, Agile mindset, Client-Focused mindset</p>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
